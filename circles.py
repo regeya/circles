40f50ab839d3ea0e7496536a664e8ed254bfaa42
regeya <regeya@gmail.com>
2025-04-13 16:49:51 -0500

    reverted back to not using ModernGL, just PyGame

--- circles.py
+++ circles.py
@@ -47,6 +47,108 @@ CGA = [
     "#FFFFFF"
 ] # 16 colors of the CGA palette.
 
+<<<<<<< Updated upstream
+=======
+pygame.init()
+
+FPS=30
+clock = pygame.time.Clock()
+
+VIRTUAL_RES=(320, 240)
+REAL_RES=(960, 720)
+
+screen = pygame.Surface(VIRTUAL_RES).convert((255, 65280, 16711680, 0))
+pygame.display.set_mode(REAL_RES, DOUBLEBUF|OPENGL)
+
+ctx = moderngl.create_context()
+
+texture_coordinates = [0, 1,  1, 1,
+                       0, 0,  1, 0]
+
+world_coordinates = [-1, -1,  1, -1,
+                     -1,  1,  1,  1]
+
+render_indices = [0, 1, 2,
+                  1, 2, 3]
+
+prog = ctx.program(
+    vertex_shader='''
+#version 330
+in vec2 vert;
+in vec2 in_text;
+out vec2 v_text;
+void main() {
+   gl_Position = vec4(vert, 0.0, 1.0);
+   v_text = in_text;
+}
+''',
+fragment_shader='''
+#version 330
+precision mediump float;
+uniform sampler2D Texture;
+uniform sampler2D mpass_texture;
+uniform vec2 color_texture_pow2_sz;
+uniform vec2 color_texture_sz;
+
+#define CRTgamma 2.5
+#define display_gamma 2.2
+#define TEX2D(c) pow(texture2D(mpass_texture, (c)), vec4(CRTgamma))
+
+out vec4 color;
+in vec2 v_text;
+
+void main() {
+  vec2 center = vec2(0.5, 0.5);
+  vec2 off_center = v_text - center;
+  //vec2 xy = gl_TexCoord[0].st;
+
+  off_center *= 1.0 + 0.5 * pow(abs(off_center.yx), vec2(5.5));
+
+  vec2 v_text2 = center+off_center;
+
+  if (v_text2.x > 1.0 || v_text2.x < 0.0 ||
+      v_text2.y > 1.0 || v_text2.y < 0.0){
+    color=vec4(0.0, 0.0, 0.0, 1.0);
+  } else {
+    color = vec4(texture(Texture, v_text2).rgb, 1.0);
+    float fv = fract(v_text2.y * float(textureSize(Texture,0).y));
+    fv=min(1.0, 0.8+0.5*min(fv, 1.0-fv));
+    color.rgb*=fv;
+  }
+}
+''')
+
+screen_texture = ctx.texture(
+    VIRTUAL_RES, 3,
+    pygame.image.tostring(screen, "RGB", 1))
+
+screen_texture.repeat_x = False
+screen_texture.repeat_y = False
+
+vbo = ctx.buffer(struct.pack('8f', *world_coordinates))
+uvmap = ctx.buffer(struct.pack('8f', *texture_coordinates))
+ibo= ctx.buffer(struct.pack('6I', *render_indices))
+
+vao_content = [
+    (vbo, '2f', 'vert'),
+    (uvmap, '2f', 'in_text')
+]
+
+vao = ctx.vertex_array(prog, vao_content, ibo)
+
+def render():
+    texture_data = screen.get_view('1')
+    screen_texture.write(texture_data)
+    ctx.clear(14/255,40/255,66/255)
+    screen_texture.use()
+    vao.render()
+    pygame.display.flip()
+
+#MAIN LOOP
+
+done=False
+
+>>>>>>> Stashed changes
 for r in (math.radians(i) for i in range(0,360,10)):
     # A little explanation here.  'for' loops in Python loop through iterables.  Here I have nested iterables.
     # range() is itself an iterable, returning the next number in the list every time the loop repeats until
